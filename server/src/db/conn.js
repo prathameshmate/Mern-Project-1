7d225e1ce427a2e3d41f4d1f06dedebb93ffdc1c
Prthmesh Mate <Prthmesh.Mate@winjittechnologiespvtltd.local>
2025-06-05 18:17:36 +0530

    Revert "change connection file"
    This reverts commit d4d27364a86a27999b691d78bcb915e3f1fd50e1.

--- server/src/db/conn.js
+++ server/src/db/conn.js
@@ -2,14 +2,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose
-  .connect(process.env.CONNECTION, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
+mongoose.connect(process.env.CONNECTION).then(()=>{
     console.log("nodejs - mongobd connection successfully");
-  })
-  .catch((err) => {
-    console.log("nodejs - mongodb connection failed " + err);
-  });
+}).catch((err)=>{
+    console.log("nodejs - mongodb connection failed " +err)
+})
