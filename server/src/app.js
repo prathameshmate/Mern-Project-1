b14587c749ae2fba05f910f89de44fb4425a2e61
Prthmesh Mate <Prthmesh.Mate@winjittechnologiespvtltd.local>
2025-06-05 16:38:33 +0530

    added @mui/material , added BE one more base url in cors

--- server/src/app.js
+++ server/src/app.js
@@ -8,16 +8,19 @@ import router from "./routes/router.js";
 const port = process.env.PORT || 5000;
 
 // build in middleware
+// app.use(cors()); // Allow all origins by default
 app.use(
   cors({
-    origin: "http://localhost:3000", // Only allow your frontend origin
-    methods: ["GET", "POST"],
+    origin: [
+      "http://localhost:3000",
+      "https://mern-project-1-three.vercel.app",
+    ], // Only allow your frontend origin
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, // ✅ Important for cookies
   })
 );
 app.use(express.json());
 app.use(router);
-// app.use(cors()); // Allow all origins by default
 
 app.listen(port, () => {
   console.log(`Listining to the port number ${port}`);
