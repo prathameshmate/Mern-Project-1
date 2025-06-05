d4d27364a86a27999b691d78bcb915e3f1fd50e1
Prthmesh Mate <Prthmesh.Mate@winjittechnologiespvtltd.local>
2025-06-05 17:55:40 +0530

    change connection file

--- server/src/db/conn.js
+++ server/src/db/conn.js
@@ -2,8 +2,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.CONNECTION).then(()=>{
+mongoose
+  .connect(process.env.CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("nodejs - mongobd connection successfully");
-}).catch((err)=>{
-    console.log("nodejs - mongodb connection failed " +err)
-})
+  })
+  .catch((err) => {
+    console.log("nodejs - mongodb connection failed " + err);
+  });
